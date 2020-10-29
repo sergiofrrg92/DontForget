c073cf89ad1ca1766f701ca5985196c3bf2ddb32
Sergio Gimeno <gimenosg@amazon.com>
2020-10-29 15:20:16 +0000

    Many things:  - More graphic design,  - packages for activities, entities, daos and db created  - logic for entities daos and db created

---
+++ app/src/main/java/com/example/dontforget/activities/CreateNoteActivity.java
@@ -0,0 +1,26 @@
+package com.example.dontforget.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.dontforget.R;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        ImageView imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
