efeac8847fd2866a5b564730ffac8c952b7e06a3
Sergio Gimeno <gimenosg@amazon.com>
2020-10-29 15:57:28 +0000

    Database logic on.

--- app/src/main/java/com/example/dontforget/activities/MainActivity.java
+++ app/src/main/java/com/example/dontforget/activities/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.dontforget.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.dontforget.R;
-import com.example.dontforget.activities.CreateNoteActivity;
+import com.example.dontforget.database.NotesDatabase;
+import com.example.dontforget.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,5 +33,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE);
             }
         });
+
+        getNotes();
+    }
+
+    private void getNotes(){
+
+        @SuppressLint("StaticFieldLeak")
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase.getNotesDatabase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("MY_NOTES", notes.toString());
+            }
+        }
+
+        new GetNotesTask().execute();
+
     }
 }
