22438278899e1df7ef5278ce93f26dce1ea1f906
Sergiofrrg92 <sergiofrrg@gmail.com>
2020-11-22 20:17:41 +0000

    tapping the notification opens the editreminder screen but updates don´t work because the ID is still 0

--- app/src/main/java/com/example/dontforget/broadcast/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/dontforget/broadcast/ReminderBroadcastReceiver.java
@@ -1,5 +1,6 @@
 package com.example.dontforget.broadcast;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,8 +8,10 @@ import android.os.Bundle;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
 
 import com.example.dontforget.R;
+import com.example.dontforget.activities.EditReminderActivity;
 import com.example.dontforget.entities.Reminder;
 
 import java.util.Random;
@@ -20,10 +23,18 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
         Bundle args = intent.getBundleExtra("REMINDER");
         Reminder reminder = (Reminder) args.getSerializable("reminderInfo");
 
+        Intent resultIntent = new Intent(context, EditReminderActivity.class);
+        resultIntent.putExtra("REMINDER_TO_EDIT", reminder);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"ReminderChannel")
                 .setSmallIcon(R.drawable.ic_clock)
                 .setContentTitle("Don´t forget it!: " + reminder.getTitle())
                 .setContentText(reminder.getDescription())
+                .setContentIntent(resultPendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         Random randomId = new Random();
